1212863b9155de775dc0c48ee00cc8e2673f9c4f
Lukáš Kurčík <lukas.kurcik@gmail.com>
2019-11-07 23:48:43 +0100

    Maven project

---
+++ src/main/java/cz/muni/fi/xkurcik/masterthesis/Main.java
@@ -0,0 +1,12 @@
+package cz.muni.fi.xkurcik.masterthesis;
+
+/**
+ * Class Main
+ *
+ * @author Lukáš Kurčík <lukas.kurcik@gmail.com>
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Masters!");
+    }
+}
