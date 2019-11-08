e6c0fa7594c4ec3ff87345f667f1e822b9ed0da2
Lukáš Kurčík <lukas.kurcik@gmail.com>
2019-11-08 16:01:11 +0100

    Added logging

--- src/main/java/cz/muni/fi/xkurcik/masterthesis/Main.java
+++ src/main/java/cz/muni/fi/xkurcik/masterthesis/Main.java
@@ -1,12 +1,19 @@
 package cz.muni.fi.xkurcik.masterthesis;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Class Main
  *
  * @author Lukáš Kurčík <lukas.kurcik@gmail.com>
  */
 public class Main {
+    private static final Logger LOGGER = LogManager.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
         System.out.println("Hello Masters!");
+        LOGGER.info("Hello Info!");
+        LOGGER.error("Hello Error!");
     }
 }
